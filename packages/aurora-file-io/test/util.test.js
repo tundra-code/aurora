ff6fbad40d74925dac37482641601e4cf12d7f55
Kyle McCrohan <kmccrohan@zagmail.gonzaga.edu>
2017-09-17 22:57:39 -0700

    Fixed add card test

--- packages/aurora-file-io/test/util.test.js
+++ packages/aurora-file-io/test/util.test.js
@@ -1,7 +1,11 @@
-import { getAuroraDirContext, noteFileName, noteFileExt } from "../util.js";
+import {
+  getAuroraDirContext,
+  attachMetaData,
+  noteFileName,
+  noteFileExt
+} from "../util.js";
 import fs from "fs";
 import { EditorState } from "draft-js";
-import { NoteModel } from "../../aurora-note";
 
 test("noteFileExt is actually an extension", () => {
   expect(noteFileExt.charAt(0)).toBe(".");
@@ -12,8 +16,13 @@ test("getAuroraDirContext generates a real location", () => {
   expect(fs.existsSync(dir)).toBe(true);
 });
 
+test("attachMetaData gives an object with multiple keys", () => {
+  const data = attachMetaData(EditorState.createEmpty());
+  expect(Object.keys(data).length).toBeGreaterThan(1);
+});
+
 test("noteFileName ends with our file extension", () => {
-  const note = new NoteModel(EditorState.createEmpty());
+  const note = attachMetaData(EditorState.createEmpty());
   const noteFile = noteFileName(note);
   expect("." + noteFile.split(".")[1]).toBe(noteFileExt);
 });
