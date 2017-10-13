3520e0e8d1f5718153292bd8adee9d85fff41a36
Flaqueeau <econrad@zagmail.gonzaga.edu>
2017-10-13 12:38:32 -0700

    Make runnable

--- packages/aurora-core/index.js
+++ packages/aurora-core/index.js
@@ -1,17 +1,11 @@
 import React from "react";
-import Feed from "../aurora-feed";
 import { Background } from "../aurora-ui";
-import io from "../aurora-persist/io";
 
 /**
  * The main app.
  */
 function Core() {
-  return (
-    <Background className="background">
-      <Feed className="feed" persist={io} />
-    </Background>
-  );
+  return <Background className="background" />;
 }
 
 export default Core;
--- packages/aurora-ui/index.js
+++ packages/aurora-ui/index.js
@@ -1,5 +1,4 @@
 import Card from "./Card.js";
 import Background from "./Background.js";
-import NoteList from "./NoteList.js";
 
-export { Card, Background, NoteList };
+export { Card, Background };
