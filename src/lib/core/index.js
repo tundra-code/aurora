df7816de6bd6ef93832eef06206b834670e767ae
Flaqueeau <econrad@zagmail.gonzaga.edu>
2017-11-14 10:23:25 -0800

    Clean up my poor merging

--- src/lib/core/index.js
+++ src/lib/core/index.js
@@ -3,14 +3,12 @@ import { withMutations } from "../mutate";
 import Frame from "../frame";
 import { connect } from "react-redux";
 import { Themed } from "../theme";
-import { setToast } from "../../redux/actions";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
 
 class Core extends React.Component {
   constructor(props) {
     super(props);
-    this.props.dispatch(setToast("do you like cats?", "info"));
   }
 
   componentWillReceiveProps(nextProps) {
@@ -18,7 +16,10 @@ class Core extends React.Component {
   }
 
   createToast(nextProps) {
-    if (!nextProps.toast) return;
+    if (!nextProps.toast) {
+      return;
+    }
+
     if (nextProps.toast.type) {
       toast[nextProps.toast.type](nextProps.toast.message);
       return;
