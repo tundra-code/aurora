c9b5ad4eee8758f9d94647ffbbeedc12ca297cbc
Flaqueeau <econrad@zagmail.gonzaga.edu>
2017-11-14 18:53:15 -0800

    remove parts from frame

--- src/lib/frame/index.js
+++ src/lib/frame/index.js
@@ -6,7 +6,6 @@ import PropTypes from "prop-types";
 import SplitPane from "react-split-pane";
 import StyledSplitPaneContainer from "../ui/SplitPanes";
 import { FixedWidthDiv } from "../ui/util/Layouts";
-import { Menu, MenuItem, MenuCard, MenuCardList } from "../ui/Menu";
 
 const LeftFixedWidthDiv = FixedWidthDiv.extend`
   background: ${props => props.theme.colors.secondaryBackground};
@@ -16,18 +15,7 @@ const LeftFixedWidthDiv = FixedWidthDiv.extend`
 const Frame = props => (
   <StyledSplitPaneContainer>
     <SplitPane split="vertical" minSize={200} maxSize={400} defaultSize={250}>
-      <LeftFixedWidthDiv>
-        <Menu>
-          <MenuItem> Lorem </MenuItem>
-          <MenuItem active>Lorem</MenuItem>
-          <MenuCardList>
-            <MenuCard>Hey there</MenuCard>
-            <MenuCard>Hey there</MenuCard>
-            <MenuCard>Hey there</MenuCard>
-          </MenuCardList>
-          <MenuItem> Lorem </MenuItem>
-        </Menu>
-      </LeftFixedWidthDiv>
+      <LeftFixedWidthDiv />
 
       <FixedWidthDiv>
         <Container>
