b085717fc55933885884929f362fe5a6c9723166
Flaqueeau <econrad@zagmail.gonzaga.edu>
2017-11-14 19:13:05 -0800

    Fix mutations package.json bug

--- src/lib/mutate/requireMutations.js
+++ src/lib/mutate/requireMutations.js
@@ -2,13 +2,20 @@ import {
   auroraMutationPackageJSONPath,
   auroraMutationFilesPath
 } from "../paths";
-
 let requireMutations = () => new Promise();
+
+const getPkg = () => {
+  try {
+    return window.require(auroraMutationPackageJSONPath());
+  } catch (error) {
+    return { dependencies: {} };
+  }
+};
 
 if (process.env.NODE_ENV !== "test") {
   requireMutations = () => {
     try {
-      const pkg = window.require(auroraMutationPackageJSONPath());
+      const pkg = getPkg();
       const modules = Object.keys(pkg.dependencies).map(
         mut => window.require(auroraMutationFilesPath(mut)) // window.require is electron's built in require, not webpacks
       );
